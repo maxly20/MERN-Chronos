245a65872d05292eae1d0ba4155fb2733d99a559
maxly20 <maxly2011@gmail.com>
2021-05-24 11:52:23 +1000

    prepare to deploy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const productRoutes = require('./routes/productRoutes');
 const connectDB = require('./config/db');
+const path = require('path');
 
 connectDB();
 
@@ -14,6 +15,13 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
